ae78b8073464c63b24eec0e4bf423cb1e6c4af7b
robertosmpm <105151878+robertosmpm@users.noreply.github.com>
2022-05-12 21:10:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    if (principal <= 0 ){
+        alert("Enter apositive number")
+    }else{
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal*years*rate/100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerText="If you deposit "+ principal +" at an interest of " + rate + "you will recive an amount of " + interest + " in the year " + year
-}
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+}};
 function updateRate()
-    {
-        var rateval = document.getElementById("rate").value;
-        document.getElementById("rate_val").innerText=rateval;
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
    
-    }
-
-        
+};
